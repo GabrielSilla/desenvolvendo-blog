c10e49eaab907a4968e79e46b2090dc5764eb3b0
Gabriel Silla <BRMOU575655@ambev.com.br>
2023-03-07 15:26:18 -0300

    Ajustes visuais

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -53,40 +53,57 @@ export default function Home() {
 
     return (
         <div className="content">
-            <div className="logo">        
-                <img src={"src/assets/img/logo.png"} />
-                <img src={"src/assets/img/design.png"} />
+            <div className="logo">
+                <img src={"src/assets/img/logo-alt.png"} />
             </div>
             <Divider />
             <div className="posts-box">
-                <Typography style={{fontWeight: 100}} variant="h4">
+                <Typography style={{fontWeight: 100, marginBottom: '10px'}} variant="h5">
                     Últimas Postagens
                 </Typography>
-                
-                <div className="posts">
-                    {posts.map((object, i) => 
-                        <div key={i}>
-                            <Card className="cards" sx={{ maxWidth: 400 }}>
-                                <CardActionArea>
-                                    <CardMedia
-                                    component="img"
-                                    height="250"
-                                    image={object.image}/>
-                                    <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {object.title}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {object.description}
-                                    </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </div>
-                    )}
+                <Divider/>
+                <div className="container">
+                    <div className="posts">
+                        {posts.map((object, i) => 
+                            <div key={i}>
+                                <Card className="cards" sx={{ maxWidth: 500 }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                        component="img"
+                                        height="250"
+                                        image={object.image}/>
+                                        <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {object.title}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {object.description}
+                                        </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
             <Divider />
+            <div className="footer">
+                <img src={"src/assets/img/simple-logo.png"} />
+                <div className="footer-text">
+                    <Typography style={{fontWeight: 700}} variant="body1" color="text.primary">
+                        2023, Desenvolvido por Gabriel Silla & Karina Silla.
+                    </Typography>
+                    <Typography variant="body2" color="text.primary">
+                        Apoiando novas carreiras.
+                    </Typography>
+                </div>
+            </div>
+            <div className="footer-end">
+                <div className="footer-end-y"></div>
+                <div className="footer-end-o"></div>
+                <div className="footer-end-r"></div>
+            </div>
         </div>
     );
   }
