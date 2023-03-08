c442d2f31141617adc1834e830d666560a6a07fa
Gabriel Silla <BRMOU575655@ambev.com.br>
2023-03-08 12:24:31 -0300

    Adicionando chips aos posts

---
+++ src/models/chip-type.ts
@@ -0,0 +1,5 @@
+export const chipTypes = {
+    back: {type: "back", label: "Backend"},
+    front: {type: "front", label: "Frontend"},
+    others: {type: "others", label: "Outros"}
+};
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,8 +1,9 @@
 ;import "./home.css";
-import { Divider, Typography, Card, CardContent, CardActionArea, CardMedia, Button, SwipeableDrawer } from "@mui/material";
+import { Divider, Typography, Card, CardContent, CardActionArea, CardMedia, Button, SwipeableDrawer, Chip } from "@mui/material";
 import { FilterAlt } from "@mui/icons-material";
 import { useState, Fragment } from "react";
 import React from "react";
+import { chipTypes } from "../../models/chip-type";
 
 
 export default function Home() {
@@ -13,47 +14,38 @@ export default function Home() {
         {
             title: "Minimal API Tutorial com .Net 7",
             description: "Aprenda a construir do zero, uma API em .Net 7 usando conceito mínimo e aplicando conhecimentos REST",
-            image: '/src/assets/posts-tests/net.png'
+            image: '/src/assets/posts-tests/net.png',
+            type: chipTypes.back
         },
         {
             title: "TDD na Prática",
             description: "Já usou o TDD? Não? Venha entender como ele funciona na prática, aplicando ao desenvolvimento.",
-            image: '/src/assets/posts-tests/TDD.png'
+            image: '/src/assets/posts-tests/TDD.png',
+            type: chipTypes.back
         },
         {
             title: "A arte do debbuging",
             description: "Melhores práticas para encontrar os erros com mais facilidade. Chega de sofrer!",
-            image: '/src/assets/posts-tests/debug.png'
+            image: '/src/assets/posts-tests/debug.png',
+            type: chipTypes.others
         },
         {
             title: "Comunicando o Python com DB",
             description: "Aprenda e entenda como funciona o Driver do SQL dentro do Python. Bora botar a mão na massa",
-            image: '/src/assets/posts-tests/sql.png'
+            image: '/src/assets/posts-tests/sql.png',
+            type: chipTypes.back
         },
         {
             title: "Vamos aprender um café?",
             description: "Eai, bateu o sono? Vamos ensinar uma receitinha pra você continuar codando com energia de sobra!",
-            image: '/src/assets/posts-tests/mocha.jpg'
+            image: '/src/assets/posts-tests/mocha.jpg',
+            type: chipTypes.others
         },
         {
             title: "Meu primeiro APP em React",
             description: "Vamos aprender um pouco de front-end agora. Vamos criar do zero um app usando o react",
-            image: '/src/assets/posts-tests/react.png'
-        },
-        {
-            title: "Minimal API Tutorial com .Net 7",
-            description: "Aprenda a construir do zero, uma API em .Net 7 usando conceito mínimo e aplicando conhecimentos REST",
-            image: '/src/assets/posts-tests/net.png'
-        },
-        {
-            title: "TDD na Prática",
-            description: "Já usou o TDD? Não? Venha entender como ele funciona na prática, aplicando ao desenvolvimento.",
-            image: '/src/assets/posts-tests/TDD.png'
-        },
-        {
-            title: "A arte do debbuging",
-            description: "Melhores práticas para encontrar os erros com mais facilidade. Chega de sofrer!",
-            image: '/src/assets/posts-tests/debug.png'
+            image: '/src/assets/posts-tests/react.png',
+            type: chipTypes.front
         }
     ];
 
@@ -78,6 +70,7 @@ export default function Home() {
                             <div key={i}>
                                 <Card className="cards" sx={{ maxWidth: 400 }}>
                                     <CardActionArea>
+                                        <Chip className={`custom-chip ${object.type.type}`} label={object.type.label} color="primary" variant="outlined" />
                                         <CardMedia
                                         component="img"
                                         height="250"
